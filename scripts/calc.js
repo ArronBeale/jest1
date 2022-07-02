e7d70f5fd9ad8dd64d4e82aee676157b9d709a2f
Arron <beale86@msn.com>
2022-07-02 13:01:51 +0000

    Add functions obj to cal.js, add subtract, multiply and division functions, add tests for subtraact, multiply and divide functions

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,17 @@
-function addition(num1, num2) {
-    return num1 + num2;
-};
+const functions = {
 
-module.exports = addition;
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+}
+
+// function addition(num1, num2) {
+//     return num1 + num2;
+// };
+
+// function subtract(num1, num1) {
+//     return num1 - num1;
+// };
+
+module.exports = functions;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,27 +1,46 @@
 /**
  * @jest-environment jsdom
  */
-const addition = require("../calc");
+const functions = require("../calc");
+// const subtract = require("../calc");
 
 describe ("calculator", () => {
     describe("addition function", () => {
         test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42);
+            expect(functions.add(20, 22)).toBe(42);
         });
         test("should return 73 for 41 + 31", () => {
-            expect(addition(42, 31)).toBe(73);
+            expect(functions.add(42, 31)).toBe(73);
+        })
+        test("Adds 5 + 5 to NOT equal 11", () => {
+            expect(functions.add(5, 35)).not.toBe(11);
         })
     })
 
-    describe("subtraction function", () => {
-        
+    describe("subtract function", () => {
+        test("should return 1 for 10 - 9", () => {
+            expect(functions.subtract(10, 9)).toBe(1);
+        });
+        test("should return 10 for 100 - 90", () => {
+            expect(functions.subtract(100, 90)).toBe(10);
+        });
     })
 
     describe("multiply function", () => {
-        
+        test("should return 9 for 3 * 3", () => {
+            expect(functions.multiply(3, 3)).toBe(9);
+        });
+        test("should return 100 for 20 * 5", () => {
+            expect(functions.multiply(20, 5)).toBe(100);
+        });
     })
 
     describe("division function", () => {
-        
+        test("should return 10 for 100 / 10", () => {
+            expect(functions.divide(100, 10)).toBe(10);
+        });
+        test("should return 9 for 90 / 10", () => {
+            expect(functions.divide(90, 10)).toBe(9);
+        });
     })
 });
